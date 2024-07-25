5dd66dcc88b80470cd3bc7a74afcfa408654f3d1
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-07-25 18:13:23 +0300

    clone phone catalog

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config-react-typescript'],
   rules: {
-    'max-len': ['error', {
-      ignoreTemplateLiterals: true,
-      ignoreComments: true,
-    }],
-    'jsx-a11y/label-has-associated-control': ["error", {
-      assert: "either",
-    }],
+    'max-len': [
+      'error',
+      {
+        ignoreTemplateLiterals: true,
+        ignoreComments: true,
+      },
+    ],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        assert: 'either',
+      },
+    ],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
 };
